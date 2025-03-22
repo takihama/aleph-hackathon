32db06c9dddc0cffe4dbb5681a91f3e692a38b91
Daniel Calderón <danielcalderonuns@gmail.com>
2025-03-22 11:00:37 -0300

    add daimo pay url

--- src/components/DaimoProviders.tsx
+++ src/components/DaimoProviders.tsx
@@ -1,30 +1,30 @@
-'use client'
+"use client";
 
-import React from 'react'
-import { DaimoPayProvider, getDefaultConfig } from '@daimo/pay'
-import { QueryClient, QueryClientProvider } from '@tanstack/react-query'
-import { WagmiProvider, createConfig } from 'wagmi'
+import React from "react";
+import { DaimoPayProvider, getDefaultConfig } from "@daimo/pay";
+import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
+import { WagmiProvider, createConfig } from "wagmi";
 
 // Configure Daimo Pay with app configuration
 const config = createConfig(
   getDefaultConfig({
-    appName: 'Aleph Hackathon App',
+    appName: "Aleph Hackathon App",
     ssr: true, // Set to true if your project uses server side rendering (SSR)
-  }),
-)
+  })
+);
 
 // Create a query client for React Query
-const queryClient = new QueryClient()
+const queryClient = new QueryClient();
 
 // Define the Provider component to wrap your application
 export function DaimoProviders({ children }: { children: React.ReactNode }) {
   return (
     <WagmiProvider config={config}>
       <QueryClientProvider client={queryClient}>
-        <DaimoPayProvider>
+        <DaimoPayProvider payApiUrl={process.env.DAIMO_API_URL!} debugMode>
           {children}
         </DaimoPayProvider>
       </QueryClientProvider>
     </WagmiProvider>
-  )
-} 
+  );
+}
